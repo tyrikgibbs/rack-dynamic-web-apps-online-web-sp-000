a096144287d5710ac9b4950cdbc82bf9cf38cf21
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-30 18:53:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
-    if num_1==num_2 && num_2==num_3
+    if num_1==num_2 || num_2==num_3
       resp.write "You Win"
     else
       resp.write "You Lose"
